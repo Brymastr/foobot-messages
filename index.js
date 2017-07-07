3d724af4815177619c4a36671c1283e1007a0bf4
Brycen Dorsay <bdorsay@rbauction.com>
2017-07-07 15:49:40 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const
+  Koa = require('koa'),
+  app = new Koa(),
+  bodyParser = require('koa-bodyparser'),
+  mongoose = require('mongoose'),
+  amqp = require('amqplib');
+
+mongoose.Promise = Promise;
+
+// TODO: Move to config
+const PORT = 3002;
+const DB = 'mongodb://localhost/foobot-messages';
+const AMQP_CONNECTION = 'amqp://localhost';
+const EXCHANGE_NAME = '';  // The name of the incoming messages exchange
+const AMQP_CONNECTION_RETRY_INTERVAL = 3000;
+const AMQP_CONNECTION_ATTEMPTS = 3;
+const DB_CONNECTION_ATTEMPTS = 10;
+const DB_CONNECTION_RETRY_INTERVAL = 3000;
+
+function connect(func, attempts, interval) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      resolve(await func());
+    } catch(err) {
+      if(attempts === 1)
+        reject(new Error('Max connection attempts reached'));
+      setTimeout(async () => {
+        resolve(await connect(func, --attempts, interval));
+      }, interval);
+    }
+  });
+}
+
+async function main() {
+  app.use(require('./routes'));
+  app.use(bodyParser());
+
+  const dbConnect = async () => {
+    console.log('attempting db connection');
+    console.log('db connection successful');
+    return await mongoose.createConnection(DB, {useMongoClient: true});
+  };
+
+  const amqpConnect = async () => {
+    console.log('attempting amqp connection');    
+    const connection = await amqp.connect(AMQP_CONNECTION);
+    const channel = await connection.createChannel();
+    await channel.assertExchange(EXCHANGE_NAME, topic);
+    channel.close();
+    console.log('db connection successful');
+    return connection;
+  }
+
+  const dbConnection = await connect(dbConnect, DB_CONNECTION_ATTEMPTS, DB_CONNECTION_RETRY_INTERVAL);
+  // const amqpConnection = await connect(amqpConnect, AMQP_CONNECTION_ATTEMPTS, AMQP_CONNECTION_RETRY_INTERVAL);
+  
+  app.listen(PORT);
+  console.log('messages service running')
+}
+
+
+
+main();
---
+++ routes.js
@@ -0,0 +1,14 @@
+const Router = require('koa-router');
+const router = new Router({
+  prefix: '/messages'
+});
+
+router.get('/', async ctx => {
+  ctx.body = 'get messages for user?'
+});
+
+router.post('/', async ctx => {
+  ctx.body = `create a new message over http. Although I don't know why this would be done.`;
+});
+
+module.exports = router.routes();
